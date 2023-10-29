e9e5efd0d625dcb867da3e9709de6f04ee589f9d
Miguel31218 <miguel.chguillermo@gmail.com>
2023-10-29 17:49:40 -0500

    Commit de imagenes - división - cardas

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.indigo,
         body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           CircleAvatar(
             radius: 55.0,
             backgroundColor: Colors.white24,
-            backgroundImage: AssetImage("assets/images/persona2.jpg"),
+            backgroundImage: AssetImage("assets/images/persona1.jpeg"),
           ),
           Text(
-            "Fiorella de Fátima de Guadalupe",
+            "Fiorella Tapia Gonzales",
             style: TextStyle(
               color: Colors.white,
               fontSize: 22.00,
@@ -28,27 +29,43 @@ void main() {
               fontWeight: FontWeight.w200,
             ),
           ),
+          Divider(
+            thickness: 0.7,
+            color: Colors.white38,
+            indent: 40.0,
+            endIndent: 40.0,
+          ),
           Card(
+            margin: EdgeInsets.symmetric(horizontal: 14.0, vertical: 8.0),
             child: ListTile(
               leading: Icon(
                 Icons.phone,
+                color: Colors.indigo,
               ), // part left
               title: Text("+51 969461067"),
               subtitle: Text("Teléfono"),
               trailing: Icon(
                 Icons.check_circle_outline,
+                color: Colors.indigo,
               ), // part right
             ),
           ),
           Card(
+            margin: EdgeInsets.symmetric(
+                horizontal: 14.0,
+                vertical:
+                    8.0), // Con card damos forma a la lista en tipo tarjeta
             child: ListTile(
+              // Con esto se crea una lista en transparente solo la caja
               leading: Icon(
                 Icons.mail,
+                color: Colors.indigo,
               ), // part left
               title: Text("ebarrionuevo@gmail.com"),
               subtitle: Text("Correo electrónico"),
               trailing: Icon(
                 Icons.check_circle_outline,
+                color: Colors.indigo,
               ), // part right
             ),
           ),
